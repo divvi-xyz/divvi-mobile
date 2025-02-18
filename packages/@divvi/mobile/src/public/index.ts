6c48402795c926e8a614f45a8bbfdc42c229520f
Tom McGuire <mcgtom10@gmail.com>
2025-02-18 15:48:47 -0800

    fix: export of button types

--- packages/@divvi/mobile/src/public/index.ts
+++ packages/@divvi/mobile/src/public/index.ts
@@ -5,7 +5,7 @@
  * - Enable better tree-shaking
  * - Prevent accidental exposure of internal implementation details
  */
-export { Button, type BtnSizes, type BtnTypes, type TextSizes } from './components/Button'
+export { BtnSizes, BtnTypes, Button, TextSizes } from './components/Button'
 export { createApp } from './createApp'
 export { getFees } from './getFees'
 export { getPublicClient } from './getPublicClient'
