94f17685fdaaa9ebea7be4cbdfbb4f38c0271629
Jean Regisser <jean.regisser@gmail.com>
2025-02-20 18:27:26 +0100

    feat: ship custom metro config for easier setup for apps

--- apps/example/babel.config.js
+++ apps/example/babel.config.js
@@ -3,20 +3,6 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      [
-        'module-resolver',
-        {
-          alias: {
-            crypto: 'react-native-quick-crypto',
-            stream: 'readable-stream',
-            buffer: '@craftzdog/react-native-buffer',
-            // Temporary hack so we can directly import from the @divvi/mobile package
-            // without transpiling, we'll probably add a custom preset for this
-            '^src/(.+)$': '../../node_modules/@divvi/mobile/src/\\1',
-            '^locales$': '../../node_modules/@divvi/mobile/locales',
-          },
-        },
-      ],
       'react-native-reanimated/plugin', // NOTE: this plugin MUST be last
     ],
   }
