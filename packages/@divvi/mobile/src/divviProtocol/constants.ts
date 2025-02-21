d98c3232c9caa128b48a5dc0f0bf8ce106c29533
Kathy Luo <kathyluo18@gmail.com>
2025-02-21 11:10:17 +0100

    fix: update supported protocols

--- packages/@divvi/mobile/src/divviProtocol/constants.ts
+++ packages/@divvi/mobile/src/divviProtocol/constants.ts
@@ -3,21 +3,14 @@ import { Address } from 'viem'
 export const REGISTRY_CONTRACT_ADDRESS: Address = '0xba9655677f4e42dd289f5b7888170bc0c7da8cdc'
 
 export const supportedProtocolIds = [
-  'beefy',
-  'tether',
-  'somm',
-  'celo',
   'aerodrome',
-  'velodrome',
-  'vana',
-  'curve',
-  'farcaster',
-  'mento',
-  'yearn',
+  'allbridge',
+  'beefy',
+  'celo',
   'fonbnk',
-  'offchainlabs',
-  'euler',
-  'ubeswap',
+  'somm',
+  'vana',
+  'mento',
 ] as const
 
 export type SupportedProtocolId = (typeof supportedProtocolIds)[number]
