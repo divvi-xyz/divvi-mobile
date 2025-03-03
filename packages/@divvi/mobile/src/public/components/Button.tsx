a7b3d84832204cb1d8a2b50e91cfe963890d7319
Tom McGuire <mcgtom10@gmail.com>
2025-03-03 10:25:34 -0800

    style: remove string literal enum hack

--- packages/@divvi/mobile/src/public/components/Button.tsx
+++ packages/@divvi/mobile/src/public/components/Button.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import BaseButton, { BtnSizes, BtnTypes, ButtonProps, TextSizes } from '../../components/Button'
 
-export type ButtonSize = `${BtnSizes}`
-export type ButtonType = `${BtnTypes}`
-export type ButtonTextSize = `${TextSizes}`
+export type ButtonSize = 'small' | 'medium' | 'full'
+export type ButtonType = 'primary' | 'secondary' | 'tertiary'
+export type ButtonTextSize = 'small' | 'medium'
 
 export interface CustomButtonProps extends Omit<ButtonProps, 'size' | 'type' | 'textSize'> {
   size?: ButtonSize
