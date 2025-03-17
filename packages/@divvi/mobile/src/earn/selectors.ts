850e1e347b5b30f681b3ffd3480aa6e69a7b23c7
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2025-03-17 14:35:16 +0200

    remove selector

--- packages/@divvi/mobile/src/earn/selectors.ts
+++ packages/@divvi/mobile/src/earn/selectors.ts
@@ -2,7 +2,4 @@ import { RootState } from 'src/redux/store'
 
 export const depositStatusSelector = (state: RootState) => state.earn.depositStatus
 
-export const depositTransactionSubmittedSelector = (state: RootState) =>
-  state.earn.depositStatus === 'loading'
-
 export const withdrawStatusSelector = (state: RootState) => state.earn.withdrawStatus
