0847c72110709174328b49ab6ed9aa5ec3cb166b
Tom McGuire <Mcgtom10@gmail.com>
2025-03-18 08:36:30 -0700

    Update packages/@divvi/mobile/src/public/components/Button.tsx
    Co-authored-by: Jean Regisser <jean.regisser@gmail.com>

--- packages/@divvi/mobile/src/public/components/Button.tsx
+++ packages/@divvi/mobile/src/public/components/Button.tsx
@@ -11,13 +11,56 @@ export interface CustomButtonProps extends Omit<ButtonProps, 'size' | 'type' | '
   textSize?: ButtonTextSize
 }
 
+function toInternalBtnType(type?: ButtonType): BtnTypes | undefined {
+  if (!type) return undefined
+  switch (type) {
+    case 'primary':
+      return BtnTypes.PRIMARY
+    case 'secondary':
+      return BtnTypes.SECONDARY
+    case 'tertiary':
+      return BtnTypes.TERTIARY
+    default:
+      const _exhaustiveCheck: never = type
+      return _exhaustiveCheck
+  }
+}
+
+function toInternalBtnSize(size?: ButtonSize): BtnSizes | undefined {
+  if (!size) return undefined
+  switch (size) {
+    case 'small':
+      return BtnSizes.SMALL
+    case 'medium':
+      return BtnSizes.MEDIUM
+    case 'full':
+      return BtnSizes.FULL
+    default:
+      const _exhaustiveCheck: never = size
+      return _exhaustiveCheck
+  }
+}
+
+function toInternalTextSize(size?: ButtonTextSize): TextSizes | undefined {
+  if (!size) return undefined
+  switch (size) {
+    case 'small':
+      return TextSizes.SMALL
+    case 'medium':
+      return TextSizes.MEDIUM
+    default:
+      const _exhaustiveCheck: never = size
+      return _exhaustiveCheck
+  }
+}
+
 export function Button(props: CustomButtonProps) {
   return (
     <BaseButton
       {...props}
-      type={props.type as BtnTypes}
-      size={props.size as BtnSizes}
-      textSize={props.textSize as TextSizes}
+      type={toInternalBtnType(props.type)}
+      size={toInternalBtnSize(props.size)}
+      textSize={toInternalTextSize(props.textSize)}
     />
   )
 }
