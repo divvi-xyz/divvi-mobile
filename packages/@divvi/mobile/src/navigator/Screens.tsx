d5cf1b8c0225f12a1c83a4011377400e20af431b
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2025-03-25 14:34:17 +0200

    feat: add initial skeleton for `EarnWithdrawConfirmationScreen` (#173)
    ### Description
    This PR adds a new `EarnWithdrawConfirmationScreen` component and a
    corresponding test file. I am creating a new component instead of
    reworking `EarnConfirmationScreen` for two reasons:
    1. This way we can iterate on the implementation step-by-step without
    forcing the rework on the user before the whole screen is complete
    2. This approach makes it a lot easier to review and go deep into
    details
    
    In general:
    - added new screen
    - added summary items for withdrawing, reward claiming and pool info
    - when the pool provider is pressed it opens pool `manageUrl` (if not
    available – opend `termsUrl`)
    ### Test plan
    - reused mocked data from `EarnConfirmationScreen.test.tsx`
    - added tests to test the initial structure of the screen header and
    summary items
    - added test for pressing the provider name
    
    | Before | After |
    |-|-|
    | **Partial Withdraw** <img
    src="https://github.com/user-attachments/assets/5bc51975-3f28-4978-93c6-ea211aaaf224"
    width="350px" /> | **Partial Withdraw** <img
    src="https://github.com/user-attachments/assets/87d5f018-ba7f-452c-ac4d-57245588aaec"
    width="350px" />
    | **Exit** <img
    src="https://github.com/user-attachments/assets/7268c41a-fee9-4c33-865c-9183e7e80b7e"
    width="350px" /> | **Exit** <img
    src="https://github.com/user-attachments/assets/55ba12dd-e487-4a4c-bafd-e6677aea4ca3"
    width="350px" /> |
    | **Reward Claim** <img
    src="https://github.com/user-attachments/assets/608e9030-531a-437e-8cde-1785f2dda18a"
    width="350px" /> | **Reward Claim** <img
    src="https://github.com/user-attachments/assets/76b2fe1e-b768-4d2f-b0e1-65cab2783f93"
    width="350px" /> |
    | **Withdraw without claiming** <img
    src="https://github.com/user-attachments/assets/27ecf708-d1a2-4e6e-b702-c3151d34a8e4"
    width="350px" /> | **Withdraw without claiming** <img
    src="https://github.com/user-attachments/assets/f243c5c1-b21e-485a-aaa3-20df25eb66fd"
    width="350px" /> |
    
    
    ### Related issues
    
    - Relates to
    [RET-223](https://linear.app/valora/issue/ENG-223/%5Bwallet%5D-add-new-reviewtransaction-component-to-earnconfirmationscreen)
    ### Backwards compatibility
    Yes
    
    ### Network scalability
    
    If a new NetworkId and/or Network are added in the future, the changes
    in this PR will:
    
    - [x] Continue to work without code changes, OR trigger a compilation
    error (guaranteeing we find it when a new network is added)

--- packages/@divvi/mobile/src/navigator/Screens.tsx
+++ packages/@divvi/mobile/src/navigator/Screens.tsx
@@ -19,6 +19,7 @@ export enum Screens {
   EarnEnterAmount = 'EarnEnterAmount',
   EarnConfirmationScreen = 'EarnConfirmationScreen',
   EarnDepositConfirmationScreen = 'EarnDepositConfirmationScreen',
+  EarnWithdrawConfirmationScreen = 'EarnWithdrawConfirmationScreen',
   EarnHome = 'EarnHome',
   TabEarn = 'TabEarn',
   EarnPoolInfoScreen = 'EarnPoolInfoScreen',
