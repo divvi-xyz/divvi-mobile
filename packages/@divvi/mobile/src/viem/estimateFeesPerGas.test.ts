ddf8aff86e0df0a04040d4e611e881d0a4232e7d
Jean Regisser <jean.regisser@gmail.com>
2025-03-27 14:47:13 +0100

    fix: estimateFeesPerGas without getGasPriceMinimum on Celo (#188)
    ### Description
    
    This updates `estimateFeesPerGas` to use Celo's fee estimation
    implementation in viem without relying on `getGasPriceMinimum` which
    can't be used anymore following the L2 hardfork.
    
    The added benefit is that it now follows the
    [baseFeeMultiplier](https://rc.viem.sh/docs/clients/chains.html#fees-basefeemultiplier)
    applied by viem and should be more future proof.
    
    The `baseFeePerGas` is captured in a hacky way, but unit tests are
    present to ensure it will keep working as expected.
    
    Viem was updated so we get the latest implementation for
    `estimateFeesPerGas` on Celo L2.
    
    ### Test plan
    
    - Updated tests
    - Manually tested the network fees are correct with different fee
    currencies.
    
    ### Related issues
    
    - N/A
    
    ### Backwards compatibility
    
    Yes
    
    ### Network scalability
    
    If a new NetworkId and/or Network are added in the future, the changes
    in this PR will:
    
    - [x] Continue to work without code changes, OR trigger a compilation
    error (guaranteeing we find it when a new network is added)

--- packages/@divvi/mobile/src/viem/estimateFeesPerGas.test.ts
+++ packages/@divvi/mobile/src/viem/estimateFeesPerGas.test.ts
@@ -1,27 +1,23 @@
 import { estimateFeesPerGas } from 'src/viem/estimateFeesPerGas'
 import networkConfig from 'src/web3/networkConfig'
 import { Block } from 'viem'
-import {
-  estimateFeesPerGas as defaultEstimateFeesPerGas,
-  getBlock,
-  readContract,
-} from 'viem/actions'
+import { getBlock } from 'viem/actions'
+import { mainnet } from 'viem/chains'
 
 jest.mock('viem/actions', () => ({
+  ...jest.requireActual('viem/actions'),
   getBlock: jest.fn(),
-  estimateFeesPerGas: jest.fn(),
-  readContract: jest.fn(),
 }))
 
 beforeEach(() => {
   jest.clearAllMocks()
+  jest.mocked(getBlock).mockResolvedValue({ baseFeePerGas: BigInt(50) } as Block)
 })
 
 describe(estimateFeesPerGas, () => {
   it('should return the correct fees per gas on Celo', async () => {
-    jest.mocked(readContract).mockResolvedValue(BigInt(50))
     const client = {
-      chain: { id: networkConfig.viemChain.celo.id },
+      chain: networkConfig.viemChain.celo,
       request: jest.fn(async ({ method, params }) => {
         expect(params).toBeUndefined()
         if (method === 'eth_gasPrice') return '0x64' // 100 in hex
@@ -31,26 +27,19 @@ describe(estimateFeesPerGas, () => {
     }
     const fees = await estimateFeesPerGas(client as any)
     expect(fees).toEqual({
-      maxFeePerGas: BigInt(110),
+      maxFeePerGas: BigInt(70), // baseFeePerGas * 1.2 (default multiplier in viem) + maxPriorityFeePerGas
       maxPriorityFeePerGas: BigInt(10),
       baseFeePerGas: BigInt(50),
     })
-    expect(defaultEstimateFeesPerGas).not.toHaveBeenCalled()
-    expect(getBlock).not.toHaveBeenCalled()
-    expect(readContract).toHaveBeenCalledWith(
-      client,
-      expect.objectContaining({
-        functionName: 'getGasPriceMinimum',
-        args: [networkConfig.celoTokenAddress],
-      })
-    )
   })
 
   it('should return the correct fees per gas on Celo when fee currency is specified', async () => {
-    jest.mocked(readContract).mockResolvedValue(BigInt(50))
     const client = {
-      chain: { id: networkConfig.viemChain.celo.id },
+      chain: networkConfig.viemChain.celo,
       request: jest.fn(async ({ method, params }) => {
+        // Celo's estimateFeePerGas calls it to check whether we're post L2
+        // See https://github.com/wevm/viem/blob/8c425c3be08f6cedc3d8fcca9f8cfe1fe493a4a3/src/celo/fees.ts#L101-L105
+        if (method === 'eth_getCode') return '0x1'
         expect(params).toEqual(['0x123'])
         if (method === 'eth_gasPrice') return '0x64' // 100 in hex
         if (method === 'eth_maxPriorityFeePerGas') return '0xa' // 10 in hex
@@ -58,74 +47,83 @@ describe(estimateFeesPerGas, () => {
       }),
     }
     const fees = await estimateFeesPerGas(client as any, '0x123')
+    // eth_gasPrice for cel2 returns baseFeePerGas + maxPriorityFeePerGas
     expect(fees).toEqual({
-      maxFeePerGas: BigInt(110),
+      maxFeePerGas: BigInt(118), // (eth_gasPrice - eth_maxPriorityFeePerGas) * 1.2 + eth_maxPriorityFeePerGas
       maxPriorityFeePerGas: BigInt(10),
-      baseFeePerGas: BigInt(50),
+      baseFeePerGas: BigInt(90), // eth_gasPrice - eth_maxPriorityFeePerGas
     })
-    expect(defaultEstimateFeesPerGas).not.toHaveBeenCalled()
-    expect(getBlock).not.toHaveBeenCalled()
-    expect(readContract).toHaveBeenCalledWith(
-      client,
-      expect.objectContaining({
-        functionName: 'getGasPriceMinimum',
-        args: ['0x123'],
-      })
+  })
+
+  it('should throw if fee currency is specified but not able to capture baseFeePerGas', async () => {
+    const client = {
+      chain: {
+        ...networkConfig.viemChain.celo,
+        fees: {
+          // This custom estimateFeesPerGas doesn't call multiply, so it won't capture the baseFeePerGas
+          estimateFeesPerGas: jest.fn(async () => ({
+            maxFeePerGas: BigInt(130),
+            maxPriorityFeePerGas: BigInt(10),
+          })),
+        },
+      },
+      request: jest.fn(async ({ method, params }) => {
+        throw new Error(`Unknown method ${method}`)
+      }),
+    }
+    await expect(estimateFeesPerGas(client as any, '0x123')).rejects.toThrow(
+      'Unable to capture baseFeePerGas'
     )
   })
 
   it('should return the default fees per gas on other networks', async () => {
-    jest
-      .mocked(defaultEstimateFeesPerGas)
-      .mockResolvedValue({ maxFeePerGas: BigInt(110), maxPriorityFeePerGas: BigInt(10) })
-    const mockBlock = { baseFeePerGas: BigInt(50) } as Block
-    jest.mocked(getBlock).mockResolvedValue(mockBlock)
     const client = {
-      chain: { id: 1 },
+      chain: mainnet,
+      request: jest.fn(async ({ method, params }) => {
+        expect(params).toBeUndefined()
+        if (method === 'eth_maxPriorityFeePerGas') return '0xa' // 10 in hex
+        throw new Error(`Unknown method ${method}`)
+      }),
     }
     const fees = await estimateFeesPerGas(client as any)
     expect(fees).toEqual({
-      maxFeePerGas: BigInt(110),
+      maxFeePerGas: BigInt(70),
       maxPriorityFeePerGas: BigInt(10),
       baseFeePerGas: BigInt(50),
     })
-    expect(defaultEstimateFeesPerGas).toHaveBeenCalledWith(client, { block: mockBlock })
-    expect(defaultEstimateFeesPerGas).toHaveBeenCalledTimes(1)
     expect(getBlock).toHaveBeenCalledWith(client)
     expect(getBlock).toHaveBeenCalledTimes(1)
-    expect(readContract).not.toHaveBeenCalled()
   })
 
   it('should throw on other networks when fee currency is specified', async () => {
-    jest
-      .mocked(defaultEstimateFeesPerGas)
-      .mockResolvedValue({ maxFeePerGas: BigInt(110), maxPriorityFeePerGas: BigInt(10) })
-    jest.mocked(getBlock).mockResolvedValue({ baseFeePerGas: BigInt(50) } as Block)
     const client = {
-      chain: { id: 1 },
+      chain: mainnet,
+      request: jest.fn(async ({ method, params }) => {
+        expect(params).toBeUndefined()
+        if (method === 'eth_maxPriorityFeePerGas') return '0xa' // 10 in hex
+        throw new Error(`Unknown method ${method}`)
+      }),
     }
-    await expect(estimateFeesPerGas(client as any, '0x123')).rejects.toThrowError(
+    await expect(estimateFeesPerGas(client as any, '0x123')).rejects.toThrow(
       'feeCurrency is only supported on Celo'
     )
-    expect(defaultEstimateFeesPerGas).not.toHaveBeenCalled()
     expect(getBlock).not.toHaveBeenCalled()
-    expect(readContract).not.toHaveBeenCalled()
   })
 
   it('should throw on other networks if baseFeePerGas is missing', async () => {
-    jest
-      .mocked(defaultEstimateFeesPerGas)
-      .mockResolvedValue({ maxFeePerGas: BigInt(110), maxPriorityFeePerGas: BigInt(10) })
     jest.mocked(getBlock).mockResolvedValue({ hash: '0x123', baseFeePerGas: null } as any as Block)
     const client = {
-      chain: { id: 1 },
+      chain: mainnet,
+      request: jest.fn(async ({ method, params }) => {
+        expect(params).toBeUndefined()
+        if (method === 'eth_maxPriorityFeePerGas') return '0xa' // 10 in hex
+        throw new Error(`Unknown method ${method}`)
+      }),
     }
-    await expect(estimateFeesPerGas(client as any)).rejects.toThrowError(
+    await expect(estimateFeesPerGas(client as any)).rejects.toThrow(
       'missing baseFeePerGas on block: 0x123'
     )
-    expect(defaultEstimateFeesPerGas).not.toHaveBeenCalled()
     expect(getBlock).toHaveBeenCalledWith(client)
     expect(getBlock).toHaveBeenCalledTimes(1)
-    expect(readContract).not.toHaveBeenCalled()
   })
 })
