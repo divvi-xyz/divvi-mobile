ccf9310f281c8fefa6efc475aaf5d8138ade5340
Alex Bakoushin <alex@bakoush.in>
2025-05-07 15:21:49 +0200

    naming

--- apps/example/e2e/scripts/wipe-nfts.ts
+++ apps/example/e2e/scripts/wipe-nfts.ts
@@ -1,6 +1,6 @@
 import { E2E_TEST_WALLET } from './consts'
 import { wipe1155AssetsForAddress } from './utils'
 ;(async () => {
-  console.log('Wiping 1155 assets for E2E_TEST_WALLET')
+  console.log('Wiping ERC-1155 assets for E2E_TEST_WALLET')
   await wipe1155AssetsForAddress(E2E_TEST_WALLET)
 })()
