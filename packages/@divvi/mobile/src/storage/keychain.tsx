113b25784aed879672654492f5de68e00fb34d29
Tom McGuire <mcgtom10@gmail.com>
2025-09-03 15:27:29 -0700

    chore(deps): use react-native-keychain ^10.0.0

--- packages/@divvi/mobile/src/storage/keychain.tsx
+++ packages/@divvi/mobile/src/storage/keychain.tsx
@@ -1,4 +1,4 @@
-import * as Keychain from '@divvi/react-native-keychain'
+import * as Keychain from 'react-native-keychain'
 import Logger from 'src/utils/Logger'
 import { ensureError } from 'src/utils/ensureError'
 
@@ -55,7 +55,7 @@ export async function retrieveStoredItem(key: string, options: Keychain.GetOptio
   try {
     const item = await Keychain.getGenericPassword({
       service: key,
-      rules: Keychain.SECURITY_RULES.NONE,
+      accessControl: Keychain.ACCESS_CONTROL.BIOMETRY_ANY_OR_DEVICE_PASSCODE,
       ...options,
     })
     if (!item) {
