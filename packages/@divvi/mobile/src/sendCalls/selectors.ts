dfebeff8380f927b486945ddfa70ba4bd69fc4ff
Alex Bakoushin <alex@bakoush.in>
2025-10-17 18:31:26 +0200

    naming

--- packages/@divvi/mobile/src/sendCalls/selectors.ts
+++ packages/@divvi/mobile/src/sendCalls/selectors.ts
@@ -1,9 +1,9 @@
 import type { RootState } from 'src/redux/reducers'
 import type { SendCallsBatch } from 'src/sendCalls/slice'
 
-export const selectBatch = (state: RootState, id: string, now?: number) => {
+export const selectBatch = (state: RootState, id: string, notExpiredAt?: number) => {
   const batch: SendCallsBatch | undefined = state.sendCalls?.batchById?.[id]
   if (!batch) return undefined
-  if (!now) return batch
-  return batch.expiresAt > now ? batch : undefined
+  if (!notExpiredAt) return batch
+  return batch.expiresAt > notExpiredAt ? batch : undefined
 }
